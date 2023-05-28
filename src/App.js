56e650c5c9cc7e8ead012d51ac309bcb128bacf4
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-05-28 18:34:27 +0300

    task all done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import image from "./assets/images/pharmacy-otc-products-turkey-64209774.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Kuwaits Local Pharamacy</h1>
+        <p>welcome to Kuwaits Local Pharamacy</p>
+        <img src={image} alt="picture of a pharmacy" className="App"></img>
+      </div>
+    </>
   );
 }
 
