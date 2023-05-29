a0b83ff59d179000634099b20fd6470aea28a5d5
Ahmaad2001 <ahmadmamounnaqawa@hotmail.com>
2023-05-29 16:35:17 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
 import image from "./assets/images/pharmacy-otc-products-turkey-64209774.jpg";
+import Products from "./products";
 
 function App() {
+  console.log(Products[0]);
   return (
     <>
       <div className="App">
         <h1>Kuwaits Local Pharamacy</h1>
         <p>welcome to Kuwaits Local Pharamacy</p>
-        <img src={image} alt="picture of a pharmacy" className="App"></img>
+        <img src={image} alt="picture of a pharmacy" className="App" />
+        <div>
+          <img src={Products[0].image} />
+          <h1>{Products[0].Name}</h1>
+          <p>{Products[0].Price}</p>
+        </div>
+        <br />
+        <div>
+          <img src={Products[1].image} />
+          <h1>{Products[1].Name}</h1>
+          <p>{Products[1].Price}</p>
+        </div>
       </div>
     </>
   );
---
+++ src/products.js
@@ -0,0 +1,17 @@
+import img1 from "./assets/images/download.jpeg";
+import img2 from "./assets/images/Aspirin.jpeg";
+const medicine1 = {
+  Name: "Panadol",
+  Price: "5 KWD",
+  image: img1,
+};
+
+const medicine2 = {
+  Name: "Aspirin",
+  Price: "6 KWD",
+  image: img2,
+};
+
+const Products = [medicine1, medicine2];
+
+export default Products;
